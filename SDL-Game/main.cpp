46a79b0c807861d2050ce0e771e1df9cc1bcf6ad
FluffyDolphins <ryankumar1011@gmail.com>
2023-10-22 06:39:09 -0400

    Initial Commit

---
+++ SDL-Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL-Game
+//
+//  Created by Ryan Kumar on 22/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
