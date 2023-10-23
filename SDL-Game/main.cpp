02c71069ff0e482f339de30dd0dd236913999e13
FluffyDolphins <ryankumar1011@gmail.com>
2023-10-23 07:33:21 -0400

    Updated program to use textures instead of surfaces to allow for faster and more efficient hardware rendering

--- SDL-Game/main.cpp
+++ SDL-Game/main.cpp
@@ -12,13 +12,11 @@
 
 
 
-const int SCREEN_WIDTH = 600; //variables all caps because stores constant
+const int SCREEN_WIDTH = 600;
 const int SCREEN_HEIGHT = 600;
 
-SDL_Window* my_window = NULL; //SDL_Window and SDL_Surface are types defined by SDL
-SDL_Surface* my_surface = NULL; //use pointers
-SDL_Surface* image_hello_world = NULL; // Image that will load is alse of type SDL_Surface, its pointer also needs to be initialized to zero
-SDL_Surface* current_surface_display = NULL;
+//variables all caps because they store constants
+
 enum KeyboardEvents : unsigned char
 {
     DEFAULT_SURFACE,
@@ -38,120 +36,207 @@ enum FileType : unsigned char
     
 };
 
+SDL_Window* my_window = nullptr; 
 
-SDL_Surface* key_press_surfaces[PRESS_KEYBOARD_TOTAL];
+//SDL_Window and SDL_Surface are types defined by SDL
 
+SDL_Surface* my_surface = nullptr;
+SDL_Surface* current_surface_display = nullptr;
+
+// Image that will load is alse of type SDL_Surface, its pointer also needs to be initialized to zero
+
+SDL_Texture* current_texture_display = nullptr;
+SDL_Renderer* my_renderer = nullptr;
+SDL_Texture* key_press_surfaces[PRESS_KEYBOARD_TOTAL];
+
+
+//Texture are efficient, driver-specific representation of pixel data. Textures are used during hardware rendering, and are stored in VRAM opposed to regular RAM, accelerating rendering operations using GPU. Meanwhile SDL_Surface is just a struct that contains pixel information.
 
 bool init()
 {
     
-bool success{true}; //flag for function (init) is set at start
+    bool success{true}; //flag for function (init) is set at start
     
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+        
         //can also only initialize video sub system, use SDL_INIT_EVERYTHING/SDL_INIT_VIDEO
     {
         success = false;
         printf("Error initializing everything. ERROR: %s\n", SDL_GetError());
     }
-    
     else
     {
-        int sdl_image_flags = IMG_INIT_PNG; //is constant from an enum
-        // can use | (or bitwise operator)for multiple flags
-        // !(IMG_Init(sdl_image_flags) & sdl_image_flags) is only used to check is Init for PNG failed
-        if (!(IMG_Init(sdl_image_flags) == sdl_image_flags))
+        my_window = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        
+        //my_window already created when pointer was initiliazed as NULL
+        
+        if (my_window == NULL)
         {
-            printf("Error initializing SDL_Image. ERROR: %s\n", IMG_GetError());
             success = false;
+            printf("Error creating window. ERROR: %s\n", SDL_GetError());
+            success = false;
+            
         }
+        /*
+         
+         //When using surfaces instead of textures, we have to also get surface when intializing
+         
         else
         {
-            my_window = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-            //my_window already created when pointer was initiliazed as NULL
+            my_surface = SDL_GetWindowSurface(my_window);
             
-            if (my_window == NULL)
+            if (my_surface == NULL)
             {
-                success = false;
-                printf("Error creating window. ERROR: %s\n", SDL_GetError());
-                success = false;
-
+                printf("Error creating window surface. ERROR: %s\n", SDL_GetError());
             }
+         */
+            
             else
-                my_surface = SDL_GetWindowSurface(my_window);
+            {
+                my_renderer = SDL_CreateRenderer(my_window, -1, SDL_RENDERER_ACCELERATED);
+                
+                if (my_renderer == NULL)
+                {
+                    success = false;
+                    printf("Error initializing renderer. ERROR: %s\n", SDL_GetError());
+                    
+                }
+                else
+                {
+                    SDL_SetRenderDrawColor(my_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+                    int sdl_image_flags = IMG_INIT_PNG;
+                    // can use | (or bitwise operator) for multiple flags
+                    // !(IMG_Init(sdl_image_flags) & sdl_image_flags) is only used to check is Init for PNG failed
+                    if (!(IMG_Init(sdl_image_flags) == sdl_image_flags))
+                    {
+                        printf("Error initializing SDL_Image. ERROR: %s\n", IMG_GetError());
+                        success = false;
+                    }
+                }
+            }
         }
-    }
     
     return success;
 }
 
-SDL_Surface* load_surface(std::string path, unsigned char file_type)
+
+SDL_Texture* load_texture(std::string path, unsigned char file_type)
+
 {
-    SDL_Surface* loaded_surface = NULL;
-    SDL_Surface* optimized_surface = NULL;
+    
+    SDL_Texture* loaded_texture = nullptr;
+    SDL_Surface* loaded_surface = nullptr;
+    
+    // We first load surface, and then create texture from surface pixels (instead of converting it into display format, which we do with optimiced_surface and SDL_ConvertSurface)
+    
     switch (file_type)
     {
         case TYPE_BMP:
-        loaded_surface = SDL_LoadBMP(path.c_str());
-            // what does path.c_str() do?
-            
+            loaded_surface = SDL_LoadBMP(path.c_str());
+            //.c_str() converts string to array with \0 (c-style string)
             break;
             
         case TYPE_PNG:
-        loaded_surface = IMG_Load(path.c_str());
+            loaded_surface = IMG_Load(path.c_str());
             break;
     }
     if (loaded_surface == NULL)
     {
-       printf("Error loading surface with path %s. ERROR: %s\n. ERROR: %s\n", path.c_str(), SDL_GetError(), IMG_GetError());
+        printf("Error loading surface with path %s. ERROR: %s\n. ERROR: %s\n", path.c_str(), SDL_GetError(), IMG_GetError());
         // takes const char* as parameter type
     }
     else
     {
-        optimized_surface = SDL_ConvertSurface(loaded_surface, my_surface->format, 0);
-        // -> helps to access format in struct my_surface easily, since my_surface is a pointer and not actually a object (allocated on heap).
-        //Same as (*my_surface).format
-        if (optimized_surface == NULL)
+        loaded_texture = SDL_CreateTextureFromSurface(my_renderer, loaded_surface);
+        if (loaded_texture == NULL)
         {
-            printf("Error optimizing surface with path %s. ERROR: %s\n. ERROR: %s\n", path.c_str(), SDL_GetError(), IMG_GetError());
+            printf("Error loading texture with path %s, ERROR: %s\n",path.c_str(), SDL_GetError());
+            
         }
-        
         SDL_FreeSurface(loaded_surface);
-    }
-
-    return optimized_surface;
+   }
     
+    return loaded_texture;
+
 }
 
+/*
+SDL_Surface* load_surface(std::string path, FileType file_type)
+{
+    SDL_Surface* loaded_surface = nullptr;
+    SDL_Surface* optimized_surface = nullptr;
+    
+    switch (file_type)
+    {
+        case TYPE_BMP:
+            loaded_surface = SDL_LoadBMP(path.c_str());
+            //.c_str() converts string to array with \0 (c-style string)
+            break;
+            
+        case TYPE_PNG:
+            loaded_surface = IMG_Load(path.c_str());
+            break;
+            
+        default:
+            printf("Error loading surface with path %s.\n Type is invalid", path.c_str());
+            return nullptr;
+            
+    }
+    
+    if (loaded_surface == NULL)
+    {
+        printf("Error loading surface with path %s. ERROR: %s\n. ERROR: %s\n", path.c_str(), SDL_GetError(), IMG_GetError());
+        // takes const char* as parameter type
+    }
+    else
+        {
+            optimized_surface = SDL_ConvertSurface(loaded_surface, my_surface->format, 0);
+            // -> helps to access format in struct my_surface easily, since my_surface is a pointer and not actually an object (allocated on heap)
+            // sometimes we only have a pointer to a class or struct (maybe allocated on heap), to access members we use -> instead of dereferencing pointer
+            // so it would be same asame as (*my_surface).format
+            if (optimized_surface == NULL)
+            {
+                printf("Error optimizing surface with path %s. ERROR: %s\n. ERROR: %s\n", path.c_str(), SDL_GetError(), IMG_GetError());
+            }
+      
+            
+        SDL_FreeSurface(loaded_surface);
+        }
+    
+ return optimized_surface;
 
-bool load_media()
+}
+*/
+
+bool load_media() //can be used for textures or surfaces
 {
     bool success = true;
-    key_press_surfaces[DEFAULT_SURFACE] = load_surface("loaded.png", TYPE_PNG);
+    key_press_surfaces[DEFAULT_SURFACE] = load_texture("loaded.png", TYPE_PNG);
     if (key_press_surfaces[DEFAULT_SURFACE] == NULL)
     {
         printf("Failed to load default image");
         success = false;
     }
-    key_press_surfaces[PRESS_KEYBOARD_UP] = load_surface("up.bmp", TYPE_BMP);
+    key_press_surfaces[PRESS_KEYBOARD_UP] = load_texture("up.bmp", TYPE_BMP);
     if (key_press_surfaces[PRESS_KEYBOARD_UP] == NULL)
     {
         printf("Failed to load up image");
         success = false;
     }
     
-    key_press_surfaces[PRESS_KEYBOARD_DOWN] = load_surface("down.bmp", TYPE_BMP);
+    key_press_surfaces[PRESS_KEYBOARD_DOWN] = load_texture("down.bmp", TYPE_BMP);
     if ( key_press_surfaces[PRESS_KEYBOARD_DOWN] == NULL)
     {
         printf("Failed to load down image");
         success = false;
     }
-    key_press_surfaces[PRESS_KEYBOARD_LEFT] = load_surface("left.bmp", TYPE_BMP);
+    key_press_surfaces[PRESS_KEYBOARD_LEFT] = load_texture("left.bmp", TYPE_BMP);
     if (key_press_surfaces[PRESS_KEYBOARD_LEFT] == NULL)
     {
         printf("Failed to load left image");
         success = false;
     }
-    key_press_surfaces[PRESS_KEYBOARD_RIGHT] = load_surface("right.bmp", TYPE_BMP);
+    key_press_surfaces[PRESS_KEYBOARD_RIGHT] = load_texture("right.bmp", TYPE_BMP);
     if (key_press_surfaces[PRESS_KEYBOARD_RIGHT] == NULL)
     {
         printf("Failed to load right image");
@@ -163,14 +248,25 @@ bool load_media()
 
 void close()
 {
+    // Destroyed all LOADED surfaces and textures
+    
     for (int i = 0; i < PRESS_KEYBOARD_TOTAL; i++)
     {
-        SDL_FreeSurface(key_press_surfaces[i]);
+        SDL_DestroyTexture(key_press_surfaces[i]);
         key_press_surfaces[i] = NULL;
     }
-    // Destroyed all LOADED surfaces
-    SDL_DestroyWindow(my_window); //Takes care of destroying my_screen_surface
-    IMG_Quit(); //Just like you initialize them, you have to quit them
+    
+    // Destroy Windows and Renderer, set pointers to NULL
+    
+    SDL_DestroyWindow(my_window);
+    //Takes care of destroying my_screen_surface/my_current_texture
+    SDL_DestroyRenderer(my_renderer);
+    my_window = NULL;
+    my_renderer = NULL;
+    
+    //Just like you initialize them, you have to quit SDL subsystems
+    
+    IMG_Quit();
     SDL_Quit();
 }
 
@@ -192,24 +288,22 @@ int main(int argc, char* args[])
        {
            bool quit = false; //Main loop flag
            
-           SDL_Rect crop_image;
-           crop_image.x = 0; // x and y start from top left corner, goes to the right and downwards as x and y increase
-           crop_image.y = 0;
-           crop_image.h = 400;
-           crop_image.w = 400;
+           SDL_Rect crop_rect; // rectangle to be copied
+           // x and y start from top left corner, goes to the right and downwards as x and y increase
+           crop_rect.x = 40; // starting x and y
+           crop_rect.y = 50;
+           crop_rect.h = 400;
+           crop_rect.w = 500;
 
-           SDL_Rect stretch_rect;
+           SDL_Rect stretch_rect; // rectangle that surface is copied into
            stretch_rect.x = 0;
            stretch_rect.y = 0;
            stretch_rect.h = SCREEN_HEIGHT;
            stretch_rect.w = SCREEN_WIDTH;
            
            SDL_Event event;
-           current_surface_display = key_press_surfaces[DEFAULT_SURFACE];
-           SDL_BlitScaled (current_surface_display, &crop_image, my_surface, &stretch_rect);
-           //SDL_BlitSurface(current_surface_display, NULL, my_surface, NULL);
-           SDL_UpdateWindowSurface(my_window);
-           
+           current_texture_display = key_press_surfaces[DEFAULT_SURFACE];
+        
            while (quit != true)
            {
                while (SDL_WaitEvent(&event)) //event handler, feed memory address of event to PollEvent. Takes in pointer of an event
@@ -223,35 +317,42 @@ int main(int argc, char* args[])
                        switch(event.key.keysym.sym) // key symbol, accessing other unions in union?
                        {
                            case SDLK_UP:
-                               current_surface_display = key_press_surfaces[PRESS_KEYBOARD_UP];
+                               current_texture_display = key_press_surfaces[PRESS_KEYBOARD_UP];
                                break;
                                
                            case SDLK_DOWN:
-                               current_surface_display = key_press_surfaces[PRESS_KEYBOARD_DOWN];
+                               current_texture_display = key_press_surfaces[PRESS_KEYBOARD_DOWN];
                                break;
                                
                            case SDLK_LEFT:
-                               current_surface_display = key_press_surfaces[PRESS_KEYBOARD_LEFT];
+                               current_texture_display = key_press_surfaces[PRESS_KEYBOARD_LEFT];
                                break;
                                
                            case SDLK_RIGHT:
-                               current_surface_display = key_press_surfaces[PRESS_KEYBOARD_RIGHT];
+                               current_texture_display = key_press_surfaces[PRESS_KEYBOARD_RIGHT];
                                break;
                                
                        }
                        
                    }
                    
-                   // can update surface in SDL_POLLEVENT to optimize (only updates if event happens)
-                   SDL_BlitScaled (current_surface_display, &crop_image, my_surface, &stretch_rect);
-                   //SDL_BlitSurface(current_surface_display, NULL, my_surface, NULL);
+                   SDL_RenderClear(my_renderer);
+                   //clears target to set colour
+                   SDL_RenderCopy(my_renderer, current_texture_display, &crop_rect, &stretch_rect);
+                   //draws texture to hidden target (according to associated rendering settings and the crop and stretch set)
+                   SDL_RenderPresent(my_renderer);
+                   //draws content of hidden target onto window associated to renderer
+                   //Note: there can't be more than one renderer
+                
+                   /*
+                    When using surfaces instead of textures:
+                    
+                   SDL_BlitScaled (current_surface_display, &crop_rect, my_surface, &stretch_rect);
+                   // or SDL_BlitSurface(current_surface_display, NULL, my_surface, NULL);
                   SDL_UpdateWindowSurface(my_window);
+                    
+                    */
                }
-               
-               //SDL_BlitScaled (current_surface_display, &crop_image, my_surface, &stretch_rect);
-               //SDL_BlitSurface(current_surface_display, NULL, my_surface, NULL);
-               //SDL_UpdateWindowSurface(my_window);
-               //only updates surface after event handler takes all events of queue and returns 0
                 
            }
        }
