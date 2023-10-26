6877a0dade24fda9b4526130e5dd93e4bfff1f15
FluffyDolphins <ryankumar1011@gmail.com>
2023-10-26 13:50:16 -0400

    Added texture class, cleaned up code and implemented color keying

---
+++ SDL-Game/my_enums.h
@@ -0,0 +1,12 @@
+//
+//  my_enums.h
+//  SDL-Game
+//
+//  Created by Ryan Kumar on 26/10/23.
+//
+
+#ifndef my_enums_h
+#define my_enums_h
+
+
+#endif /* my_enums_h */
---
+++ SDL-Game/texture_class.h
@@ -0,0 +1,12 @@
+//
+//  Texture_class.h
+//  SDL-Game
+//
+//  Created by Ryan Kumar on 26/10/23.
+//
+
+#ifndef Texture_class_h
+#define Texture_class_h
+
+
+#endif /* Texture_class_h */
