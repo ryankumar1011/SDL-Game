8e63cb4aa387974ac501d0fbeedffbb4f65102cb
FluffyDolphins <ryankumar1011@gmail.com>
2023-12-28 03:39:36 +0800

    Added Hearts class to track and show player health

---
+++ SDL-Game/hearts.cpp
@@ -0,0 +1,8 @@
+//
+//  hearts.cpp
+//  SDL-Game
+//
+//  Created by Ryan Kumar on 28/12/23.
+//
+
+#include "hearts.hpp"
---
+++ SDL-Game/hearts.h
@@ -0,0 +1,13 @@
+//
+//  hearts.hpp
+//  SDL-Game
+//
+//  Created by Ryan Kumar on 28/12/23.
+//
+
+#ifndef hearts_hpp
+#define hearts_hpp
+
+#include <stdio.h>
+
+#endif /* hearts_hpp */
